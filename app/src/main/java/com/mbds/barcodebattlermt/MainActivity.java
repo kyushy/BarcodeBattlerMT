8f93119ca96778e92c905b73ea35f305d33ef143
Fred <kyush.spr@gmail.com>
2017-10-25 11:39:13 +0200

    add MainActivity

--- app/src/main/java/com/mbds/barcodebattlermt/MainActivity.java
+++ app/src/main/java/com/mbds/barcodebattlermt/MainActivity.java
@@ -1,20 +1,38 @@
 package com.mbds.barcodebattlermt;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        layout = (RelativeLayout) findViewById(R.id.main_rlayout);
         blink();
+
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+            }
+        });
+
     }
 
     private void blink(){
@@ -22,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                int timeToBlink = 1000;    //in milissegunds
+                int timeToBlink = 1000;
                 try{Thread.sleep(timeToBlink);}catch (Exception e) {}
                 handler.post(new Runnable() {
                     @Override
