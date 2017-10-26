b8057d0872625b9d724b7f4aab2c6adebaf5fe44
Fred <kyush.spr@gmail.com>
2017-10-26 14:44:54 +0200

    Battler fragment

--- app/src/main/java/com/mbds/barcodebattlermt/activities/GameActivity.java
+++ app/src/main/java/com/mbds/barcodebattlermt/activities/GameActivity.java
@@ -1,6 +1,8 @@
 package com.mbds.barcodebattlermt.activities;
 
+import android.app.FragmentManager;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -13,16 +15,20 @@ import android.widget.Toast;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import com.mbds.barcodebattlermt.R;
+import com.mbds.barcodebattlermt.fragments.BattleListFragment;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity  {
 
     private TextView mTextMessage;
+    private FragmentManager manager;
+    private BattleListFragment fragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -46,8 +52,8 @@ public class GameActivity extends AppCompatActivity {
                     return true;
                 case R.id.navigation_monsters:
                     mTextMessage.setText(R.string.title_monsters);
-                    Intent intent = new Intent(GameActivity.this, BattlerListActivity.class);
-                    startActivity(intent);
+                    fragment = BattleListFragment.newInstance("","");
+                    manager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
                     return true;
                 case R.id.navigation_gears:
                     mTextMessage.setText(R.string.title_gears);
@@ -63,6 +69,7 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game_b);
 
         mTextMessage = (TextView) findViewById(R.id.message);
+        manager = GameActivity.this.getFragmentManager();
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
